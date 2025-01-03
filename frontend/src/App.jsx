4bc2e132fec141b32e5f17df84cdfd183256d32e
esaw1 <ethansaw@outlook.com>
2025-01-02 19:43:07 -0800

    man

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';  // Make sure this file is imported to apply custom styles
 
 function App() {
   const [count, setCount] = useState(0);
-
 
   const randomLetters = () => {
     const letters = [];
@@ -14,38 +13,38 @@ function App() {
       letters.push(randomChar);
     }
     return letters;
-  }
+  };
 
   return (
     <>
-      <div className="justify-center">
+      <div className="flex justify-center">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react"
-               alt="React logo"/>
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="text-4xl font-semibold text-center mt-8">Vite + React</h1>
+      <div className="card mt-8 text-center">
         <input
           type="range"
           min="0"
           max="26"
           value={count}
           onChange={(e) => setCount(Number(e.target.value))}
+          className="w-52"
         />
-        <output className="card__text"> {count}</output>
-        <h2>
+        <output className="flex justify-center card__text text-sm">{count}</output>
+        <h2 className="mt-4">
           {count > 0 ? randomLetters().join(' ') : '\n'}
         </h2>
       </div>
-      <p className="read-the-docs animate-bounce">
+      <p className="read-the-docs animate-bounce mt-8">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
