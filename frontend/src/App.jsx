f39f3923d307f0c358734316a8ce46a09aff7c53
esaw1 <ethansaw@outlook.com>
2025-01-02 19:35:56 -0800

    confusion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,28 +4,44 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+
+  const randomLetters = () => {
+    const letters = [];
+    for (let i = 0; i < count; i++) {
+      const randomChar = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      letters.push(randomChar);
+    }
+    return letters;
+  }
 
   return (
     <>
-      <div>
+      <div className="justify-center">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react"
+               alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <input
+          type="range"
+          min="0"
+          max="26"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <output className="card__text"> {count}</output>
+        <h2>
+          {count > 0 ? randomLetters().join(' ') : '\n'}
+        </h2>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs animate-bounce">
         Click on the Vite and React logos to learn more
       </p>
     </>
